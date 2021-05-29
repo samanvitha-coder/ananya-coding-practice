83c03446ccf90fd1b8313e3e9945ea26542b4122
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-05-30 00:43:56 +0530

    DestinationCityTest

---
+++ src/test/java/in/samanvitha/leetcode/easy/linkedlist_arrays/DestinationCityTest.java
@@ -0,0 +1,23 @@
+package in.samanvitha.leetcode.easy.linkedlist_arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+class DestinationCityTest {
+
+    @Test
+    public void InputLegalTestCase() {
+        DestinationCity dst = new DestinationCity();
+        List<List<String>> listoflists = new ArrayList<List<String>>();
+        List<String> list1 =  Arrays.asList("a","b");
+        listoflists.add(list1);
+        String result = dst.destCity(listoflists);
+        Assertions.assertEquals("b",result);
+    }
+
+}
