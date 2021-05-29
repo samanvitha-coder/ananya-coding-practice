ec577d553a8380f3e8be9086bc8f7a014eef1d6d
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-05-30 00:45:48 +0530

    SquareIsWhiteTest

---
+++ src/test/java/in/samanvitha/leetcode/easy/linkedlist_arrays/SquareIsWhiteTest.java
@@ -0,0 +1,31 @@
+package in.samanvitha.leetcode.easy.linkedlist_arrays;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SquareIsWhiteTest {
+
+    @Test
+    public void testCaseLegalInput(){
+        SquareIsWhite si = new SquareIsWhite();
+        Boolean result = si.squareIsWhite("a2");
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    public void testCasePassingEmptyStringAsInput(){
+        SquareIsWhite si = new SquareIsWhite();
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> si.squareIsWhite(""));
+        Assertions.assertEquals("Empty string is not allowed", exception.getMessage());
+    }
+
+    @Test
+    public void testCaseLengthCheck(){
+        SquareIsWhite si = new SquareIsWhite();
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> si.squareIsWhite("a11"));
+        Assertions.assertEquals("Length of the string should be 2", exception.getMessage());
+    }
+}
