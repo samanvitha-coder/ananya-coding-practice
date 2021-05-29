b60e7cdf9cc272002e828bebd43332f17016e19c
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-05-30 00:40:29 +0530

    CountNegatives

---
+++ src/test/java/in/samanvitha/leetcode/easy/linkedlist_arrays/HeightCheckerTest.java
@@ -0,0 +1,22 @@
+package in.samanvitha.leetcode.easy.linkedlist_arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HeightCheckerTest {
+    @Test
+    public void testCaseLegalInput(){
+        HeightChecker hc = new HeightChecker();
+        int result = hc.heightChecker(new int[]{1, 1, 3, 2, 1});
+        Assertions.assertEquals(2,result);
+    }
+
+    @Test
+    public void testCaseEmptyArray(){
+        HeightChecker hc = new HeightChecker();
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> hc.heightChecker(new int[]{}));
+        Assertions.assertEquals("Empty Array not allowed", exception.getMessage());
+    }
+
+}
