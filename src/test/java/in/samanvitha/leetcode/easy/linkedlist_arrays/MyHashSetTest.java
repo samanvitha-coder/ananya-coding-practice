b38545739054ee7618a3e3ce52691e01394a219a
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-06-07 23:41:00 +0530

    MyHashSetTest

---
+++ src/test/java/in/samanvitha/leetcode/easy/linkedlist_arrays/MyHashSetTest.java
@@ -0,0 +1,22 @@
+package in.samanvitha.leetcode.easy.linkedlist_arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyHashSetTest {
+    @Test
+    public void testCorrectOperation(){
+        MyHashSet ms = new MyHashSet();
+        Assertions.assertEquals(ms.add(1),true);
+        Assertions.assertEquals(ms.add(2),true);
+        Assertions.assertEquals(ms.contains(1),true);
+        Assertions.assertEquals(ms.contains(3),false);
+        Assertions.assertEquals(ms.add(2),true);
+        Assertions.assertEquals(ms.contains(2),true);
+        Assertions.assertEquals(ms.remove(2),false);
+        Assertions.assertEquals(ms.contains(2),false);
+    }
+
+}
