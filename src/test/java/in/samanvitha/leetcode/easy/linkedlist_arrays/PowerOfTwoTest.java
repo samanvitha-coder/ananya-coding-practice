a288fa13b602e942283c3afd7441027373931e08
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-06-13 09:13:23 +0530

    PowerOfTwoTest

---
+++ src/test/java/in/samanvitha/leetcode/easy/linkedlist_arrays/PowerOfTwoTest.java
@@ -0,0 +1,22 @@
+package in.samanvitha.leetcode.easy.linkedlist_arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class PowerOfTwoTest {
+    @Test
+    public void testCaseLegalInput() {
+        PowerOfTwo pt = new PowerOfTwo();
+        boolean result = pt.isPowerOfTwo(9);
+        Assertions.assertEquals(false, result);
+    }
+    @Test
+    public void testCaseLegalInputTrue() {
+        PowerOfTwo pt = new PowerOfTwo();
+        boolean result = pt.isPowerOfTwo(2);
+        Assertions.assertEquals(true, result);
+    }
+
+}
