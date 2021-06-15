a6296c067fada79ed400922d5e26608712d3f3ab
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-06-15 22:23:13 +0530

    LinkedListApplication

---
+++ src/main/java/in/samanvitha/design/linkedlist/service/LinkedListApplication.java
@@ -0,0 +1,20 @@
+package in.samanvitha.design.linkedlist.service;
+
+import in.samanvitha.design.linkedlist.model.LinkedList;
+
+public class LinkedListApplication {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+        for(int element : arr){
+            list.insertAtEnd(element);
+        }
+        System.out.println(list.deleteFront());
+        list.print();
+        System.out.println(list.deleteEnd());
+        list.print();
+        list.insertAtBeginning(11);
+        list.print();
+    }
+}
