3ce12bf6bae66649554f3c06a4340f099cfa4514
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-06-15 22:23:01 +0530

    LinkedListNode

---
+++ src/main/java/in/samanvitha/design/linkedlist/model/LinkedListNode.java
@@ -0,0 +1,22 @@
+package in.samanvitha.design.linkedlist.model;
+
+public class LinkedListNode {
+    private int value;
+    private LinkedListNode next;
+
+    public LinkedListNode(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public void setNext(LinkedListNode node){
+        this.next = node;
+    }
+
+    public LinkedListNode getNext(){
+        return this.next;
+    }
+}
