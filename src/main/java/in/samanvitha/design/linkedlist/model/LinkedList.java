d90afbd853bcc2911fd73f7c115c0cebc9add74d
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-06-20 11:47:28 +0530

    LinkedList

--- src/main/java/in/samanvitha/design/linkedlist/model/LinkedList.java
+++ src/main/java/in/samanvitha/design/linkedlist/model/LinkedList.java
@@ -1,7 +1,5 @@
 package in.samanvitha.design.linkedlist.model;
 
-import com.sun.deploy.security.SelectableSecurityManager;
-
 public class LinkedList {
 
     private LinkedListNode head;
@@ -25,6 +23,84 @@ public class LinkedList {
         }
 
     }
+    public void insertAtGivenPosition(int val,int pos){
+        LinkedListNode node = new LinkedListNode(val);
+        if(pos<1){
+            System.out.println("Invalid position");
+        }
+        if(head == null && pos != 1){
+            return;
+        }
+        if(head == null && pos==1){
+            head = node;
+        }
+        if(head != null && pos == 1){
+
+            node.setNext(head);
+            head=node;
+            return;
+        }
+
+        LinkedListNode temp = head;
+        LinkedListNode prev = null;
+        int i=1;
+        while(i<pos){
+            prev = temp;
+            temp = temp.getNext();
+            if(temp == null)
+                break;
+            i++;
+        }
+        node.setNext(temp);
+        prev.setNext(node);
+    }
+
+
+    public void insertAtEnd(int val) {
+        LinkedListNode node = new LinkedListNode(val);
+
+        if (head == null) {
+            head = node;
+        } else {
+            LinkedListNode temp = head;
+
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(node);
+        }
+
+    }
+
+
+    public void deleteAtGivenPosition(int pos){
+
+        if(head == null){
+            System.out.println("The list is empty");
+            return;
+        }
+        if(pos<1){
+            System.out.println("The position should be greater than or equal to 1");
+            return;
+        }
+        LinkedListNode temp = head;
+        if(pos == 1){
+            head = temp.getNext();
+            return;
+        }
+
+        for(int i=1; temp!=null && i < pos-1 ;i++){
+            temp = temp.getNext();
+        }
+        if (temp == null || temp.getNext() == null) {
+            return;
+        }
+
+        LinkedListNode next = temp.getNext().getNext();
+        temp.setNext(next);
+    }
+
+
 
     public boolean deleteFront(){
       if(head!= null) {
@@ -51,22 +127,53 @@ public class LinkedList {
 
     }
 
-    public void insertAtEnd(int val) {
-        LinkedListNode node = new LinkedListNode(val);
 
-        if (head == null) {
-            head = node;
-        } else {
-            LinkedListNode temp = head;
+    public int getSize(){
+        LinkedListNode temp = head;
+        int cnt=0;
+        if(head==null)
+            return 0;
+        else{
 
             while (temp.getNext() != null) {
-                temp = temp.getNext();
+                temp= temp.getNext();
+                cnt++;
             }
-            temp.setNext(node);
-        }
 
+
+        }
+        return cnt+1;
     }
 
+    public boolean check(int val){
+        if(head == null){
+            System.out.println("The given list has no nodes");
+            return false;
+        }
+        else{
+            LinkedListNode temp = head;
+            while(temp!=null){
+                if(temp.getValue() == val)
+                    return true;
+                temp = temp.getNext();
+            }
+            return false;
+        }
+    }
+
+    public  void reverse(){
+        LinkedListNode prev = null;
+        LinkedListNode current = head;
+        LinkedListNode next = null;
+        while(current != null){
+            next = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
 
     public void print() {
         System.out.print("head -> ");
@@ -83,6 +190,7 @@ public class LinkedList {
     }
 
     public Iterator iterator(){
+
         return new Iterator(head);
     }
 
@@ -90,10 +198,12 @@ public class LinkedList {
         private LinkedListNode ptr;
 
         public Iterator(LinkedListNode head){
+
             this.ptr = head;
         }
 
         public boolean hasNext(){
+
             return ptr != null;
         }
 
