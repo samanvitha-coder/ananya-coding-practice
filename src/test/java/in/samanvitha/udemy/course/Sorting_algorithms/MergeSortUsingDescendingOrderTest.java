83512e5c7e404527ff33490f692c24e84245a13f
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-09 19:34:05 +0530

    MergeSortUsingDescendingOrderTest

---
+++ src/test/java/in/samanvitha/udemy/course/Sorting_algorithms/MergeSortUsingDescendingOrderTest.java
@@ -0,0 +1,24 @@
+package in.samanvitha.udemy.course.Sorting_algorithms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortUsingDescendingOrderTest {
+
+    @Test
+    public void testCorrectOperation(){
+        MergeSortUsingDescendingOrder ms = new MergeSortUsingDescendingOrder();
+        int[] arr = {50,4,23,-8,56,9};
+
+        ms.mergeSort(arr,0,arr.length);
+        Assertions.assertEquals(arr[0],56);
+        Assertions.assertEquals(arr[1],50);
+        Assertions.assertEquals(arr[2],23);
+        Assertions.assertEquals(arr[3],9);
+        Assertions.assertEquals(arr[4],4);
+        Assertions.assertEquals(arr[5],-8);
+
+    }
+}
