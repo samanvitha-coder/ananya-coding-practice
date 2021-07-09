32d81189e19c33d670f46800e49af5147f862356
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-09 19:30:23 +0530

    InsertionSort

---
+++ src/main/java/in/samanvitha/udemy/course/Sorting_algorithms/InsertionSort.java
@@ -0,0 +1,26 @@
+package in.samanvitha.udemy.course.Sorting_algorithms;
+
+public class InsertionSort {
+    public static void main( String[] args ) {
+        InsertionSort insert = new InsertionSort();
+        int[] arr = {10,35,29,42,12,89,45};
+        int[] arr1 = insert.insertionSort(arr);
+    }
+    public int[] insertionSort(int[] arr){
+
+        for(int i=1;i< arr.length;i++){
+            int n = arr[i];
+            int j=i;
+            while(j>0 && arr[j-1]>n){
+                arr[j] = arr[j-1];
+                j=j-1;
+            }
+            arr[j] = n;
+        }
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+
+}
