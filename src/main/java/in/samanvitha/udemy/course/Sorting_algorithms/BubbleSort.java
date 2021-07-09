5d9ea2f6b2f661633664ab1c11799f78a02d677f
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-09 19:29:57 +0530

    BubbleSort

--- src/main/java/in/samanvitha/udemy/course/Sorting_algorithms/BubbleSort.java
+++ src/main/java/in/samanvitha/udemy/course/Sorting_algorithms/BubbleSort.java
@@ -1,12 +1,13 @@
-package in.samanvitha.udemy.course.Sorting_Techniques;
+package in.samanvitha.udemy.course.Sorting_algorithms;
 
 public class BubbleSort {
     public static void main( String[] args ) {
         BubbleSort bubble = new BubbleSort();
         int[] arr = {10,35,29,42,12,89,45};
-        bubble.bubbleSort(arr);
+        int[] arr1 = bubble.bubbleSort(arr);
+
     }
-    public void bubbleSort(int[] arr){
+    public int[] bubbleSort(int[] arr){
         for(int i=arr.length-1;i>0;i--){
 
             for(int j=0;j<i;j++){
@@ -22,5 +23,6 @@ public class BubbleSort {
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+        return arr;
     }
 }
