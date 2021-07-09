d6e3cb1982db531630961bad359f5405b21ac6d8
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-09 19:32:03 +0530

    ShellSort

---
+++ src/main/java/in/samanvitha/udemy/course/Sorting_algorithms/ShellSort.java
@@ -0,0 +1,28 @@
+package in.samanvitha.udemy.course.Sorting_algorithms;
+
+public class ShellSort {
+    public static void main( String[] args ) {
+        ShellSort shell = new ShellSort();
+        int[] arr = {10,35,29,42,12,89,45};
+        int[] arr1 = shell.shellSort(arr);
+    }
+
+    public int[] shellSort(int[] arr){
+
+        for(int gap=arr.length/2;gap>0;gap/=2) {
+            for(int i=gap;i<arr.length;i++) {
+                int n = arr[i];
+                int j=i;
+                while(j>=gap && arr[j-gap]>n) {
+                    arr[j] = arr[j-gap];
+                    j = j - gap;
+                }
+                arr[j] = n;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+}
