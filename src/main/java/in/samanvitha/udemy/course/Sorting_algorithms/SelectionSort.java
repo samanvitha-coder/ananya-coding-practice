457e6b1fd9617a9793a000fe1dca6143b39005d7
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-09 19:31:53 +0530

    SelectionSort

---
+++ src/main/java/in/samanvitha/udemy/course/Sorting_algorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+package in.samanvitha.udemy.course.Sorting_algorithms;
+
+public class SelectionSort {
+    public static void main( String[] args ) {
+        SelectionSort selection = new SelectionSort();
+        int[] arr = {10,35,29,42,12,89,45};
+        int[] arr1 = selection.selectionSort(arr);
+    }
+    public int[] selectionSort(int[] arr){
+
+        for(int i= arr.length-1;i>0;i--) {
+            int large = 0;
+            for(int j=0;j<=i;j++) {
+                if(arr[j]>arr[large]){
+                    large = j;
+                }
+            }
+            int temp = arr[large];
+            arr[large] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+}
