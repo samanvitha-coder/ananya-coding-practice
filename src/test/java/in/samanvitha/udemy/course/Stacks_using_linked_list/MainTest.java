2d0a4a85b2119af258088c76be19611bff11d22c
samanvitha-coder <79389500+samanvitha-coder@users.noreply.github.com>
2021-07-22 19:29:42 +0530

    MainTest

---
+++ src/test/java/in/samanvitha/udemy/course/Stacks_using_linked_list/MainTest.java
@@ -0,0 +1,23 @@
+package in.samanvitha.udemy.course.Stacks_using_linked_list;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+    @Test
+    public void testCaseInput(){
+        LinkedStack stack = new LinkedStack();
+        Assertions.assertEquals(stack.isEmpty(),true);
+        stack.push(new Employee("Jane", "Jones", 123));
+        stack.push(new Employee("John", "Doe", 4567));
+        stack.push(new Employee("Mary", "Smith", 22));
+        stack.push(new Employee("Mike", "Wilson", 3245));
+        stack.push(new Employee("Bill", "End", 78));
+        Assertions.assertEquals(stack.peek().getId(),78);
+        Assertions.assertEquals(stack.pop().getId(),78);
+        Assertions.assertEquals(stack.isEmpty(),false);
+        Assertions.assertEquals(stack.peek().getId(),3245);
+
+    }
+}
